fc78655038647686c75e94a54acaf7490d27a423
ISXDora <isinhasena@gmail.com>
2022-03-16 11:04:10 -0300

    fix: correção bd tabela updated_at notnull

---
+++ Back-end/api-webClever/src/services/GetHealthMetricsService.ts
@@ -0,0 +1,15 @@
+import { getRepository } from "typeorm";
+import { Health_Metric } from "../models/Health_metrics";
+
+
+export class GetHealthMetricsService {
+    async execute(id: string) {
+        const repo = getRepository(Health_Metric)
+
+        const health_metrics = await repo.findOne(id,{
+            relations:['user']
+        });
+
+        return health_metrics
+    }
+}
